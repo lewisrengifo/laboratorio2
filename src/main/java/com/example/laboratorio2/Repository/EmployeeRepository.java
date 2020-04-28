0452a5a925764cccb59400abc253346e1115b896
Sebastian-RG <fullmetalliferous@gmail.com>
2020-04-27 19:24:54 -0500

    Commit 2 Sebastian

--- src/main/java/com/example/laboratorio2/Repository/EmployeeRepository.java
+++ src/main/java/com/example/laboratorio2/Repository/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package com.example.laboratorio2.Repository;
 
-public interface EmployeeRepository  {
+import com.example.laboratorio2.Entity.employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<employee,String> {
 }
