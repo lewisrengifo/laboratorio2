168cf23ff2741499871cbc2da0fc6c620dc11f80
Eduardo Sanchez <a20122509@pucp.pe>
2020-04-27 19:18:05 -0500

    segundocommitEdu

--- src/main/java/com/example/laboratorio2/Entity/jobs.java
+++ src/main/java/com/example/laboratorio2/Entity/jobs.java
@@ -1,4 +1,47 @@
 package com.example.laboratorio2.Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class jobs {
+    @Id
+    private String job_id;
+    @Column(nullable = false)
+    private String job_title;
+    private int min_salary;
+    private int max_salary;
+
+    public String getJob_id() {
+        return job_id;
+    }
+
+    public void setJob_id(String job_id) {
+        this.job_id = job_id;
+    }
+
+    public String getJob_title() {
+        return job_title;
+    }
+
+    public void setJob_title(String job_title) {
+        this.job_title = job_title;
+    }
+
+    public int getMin_salary() {
+        return min_salary;
+    }
+
+    public void setMin_salary(int min_salary) {
+        this.min_salary = min_salary;
+    }
+
+    public int getMax_salary() {
+        return max_salary;
+    }
+
+    public void setMax_salary(int max_salary) {
+        this.max_salary = max_salary;
+    }
 }
